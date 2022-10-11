11d43c8a243240b5e4c293488424d87960bd1277
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-11 21:29:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -55,3 +55,65 @@ if __name__ == "__main__":
 else:
     # handles Cloud Run container termination
     signal.signal(signal.SIGTERM, shutdown_handler)
+
+ 
+import discord
+import asyncio
+import os
+import subprocess
+import html
+import pickle
+import ffmpeg
+from gtts import gTTS
+from collections import defaultdict, deque
+from pathlib import Path
+from discord import Intents
+from discord.channel import VoiceChannel
+from collections import defaultdict, deque
+from pathlib import Path
+from discord import Intents
+from dotenv import load_dotenv
+import signal
+import sys
+from types import FrameType
+from flask import Flask
+from utils.logging import logger
+app = Flask(__name__)
+load_dotenv()
+TOKEN = os.environ['TOKEN']
+
+
+intents: Intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('Login!!!')
+
+
+@client.event
+@client.event
+async def on_message(message):
+    global voiceChannel
+    if message.author.bot:
+        return
+    if message.author.voice.channel:
+        if message.content == '!con':
+            voiceChannelｌ = await VoiceChannel.connect(message.author.voice.channel)
+            await message.channel.send('読み上げるよ！')
+            return
+        if message.content == '!en':
+            VoiceChannel.stop()
+            await message.channel.send('またね！')
+            await VoiceChannel.disconnect()
+            return
+        elif message.content != '!con' or '!en':
+            tts = gTTS(message.content)
+            tts.save("hello.mp3")
+            message.guild.voice_client.play(discord.FFmpegPCMAudio("hello.mp3"))
+            return
+    else:
+        return
+
+client.run(TOKEN)
